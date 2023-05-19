f2a1c9068fa1f8d9d48078208d309acc69b51536
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2023-05-19 13:05:14 +0530

    Removed loop and using functions instead

--- tic_tac_toe.clj
+++ tic_tac_toe.clj
@@ -1,37 +1,58 @@
-(ns tic-tac-toe)
+(ns tic-tac-toe 
+  (:require [clojure.set :as clojure.set]))
 
-(def player1 {:name "player1" 
-              :symbol :X 
+(def player1 {:name "player1"
+              :symbol :X
               :moves #{}})
 
-(def player2 {:name "player2" 
-              :symbol :O 
+(def player2 {:name "player2"
+              :symbol :O
               :moves #{}})
 
 (def game {:current-player player1
            :next-player player2})
 
-
-
 (def winning-moves #{#{1 2 3} #{4 5 6} #{7 8 9} #{1 4 7} #{2 5 8} #{3 6 9} #{1 5 9} #{3 5 7}})
 
-(defn winner? [player]
-  (some #(every? (player :moves) %) winning-moves))
+(defn won? [player]
+  (some #(every? (:moves player) %) winning-moves))
 
-(defn move [] 
-    (->> (read-line)
-         (read-string)))
+(defn drawn? [moves1 moves2] 
+  (<= 9 (count (clojure.set/union moves1 moves2))))
+
+(defn move []
+  (->> (read-line)
+       (read-string)))
 
 (defn add-move [game move]
-    (update-in game [:current-player :moves] conj move))
+  (update-in game [:current-player :moves] conj move))
 
 (defn change-turn [game]
-  (let [current-player (game :current-player) 
-        next-player (game :next-player)]
+  (let [current-player (:current-player game)
+        next-player (:next-player game)]
     (assoc game :current-player next-player
-            :next-player current-player)))
+           :next-player current-player)))
 
-(loop [{:keys [next-player] :as game} game] 
-  (if (winner? next-player)
-    (println (next-player :name) "ne game khatam kr diya...")
-    (recur (change-turn (add-move game (move))))))
+(defn conduct-turn [game]
+  (let [current-player (:current-player game)
+        next-player (:next-player game)
+        current-player-moves (:moves current-player)
+        next-player-moves (:moves next-player)]
+    (cond 
+      (won? next-player) (assoc game :over true :winner next-player)
+      (drawn? current-player-moves next-player-moves) (assoc game :over true :drawn true)
+      :else (change-turn (add-move game (move))))))
+
+(defn game-not-over? [game]
+  (not (:over game)))
+
+(defn results [game]
+  (cond
+    (:winner game) (str (:name (:winner game)) " ne game khatam kr diya...")
+    (:drawn game) (str "Game drawn!!!"))
+  )
+
+(defn play-game []
+  (first (drop-while game-not-over? (iterate conduct-turn game))))
+
+(results (play-game))
